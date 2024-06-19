ca407a4d6c4529deaec993b1be35eb31b751a37c
Knifx <97795745+Knifx@users.noreply.github.com>
2024-06-19 12:54:51 +0300

    script.js

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ async function searchProducts() {
     console.log(`Запрос: ${query}`);
     
     try {
+        // Используем правильный API URL
         const url = `https://skidkaonline.ru/apiv3/products/?limit=30&offset=0&shop_id=9&city_id=72&fields=id,name,name2,shops_ids,url,noted,discount_url,discount_name,date,notalladdr,image336,imagefull,brands,pricebefore,priceafter,discount_type,discount,externalurl,countPlus,countMinus,comments,desc,color,daystitle,liked,started_today,published_today&query=${encodeURIComponent(query)}`;
         console.log(`Запрос к API: ${url}`);
         const response = await axios.get(url);
