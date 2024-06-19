45c2bb69aafa2cb94e38c41eca3dd4ac6c8b81fe
Knifx <97795745+Knifx@users.noreply.github.com>
2024-06-19 11:38:43 +0300

    script.js

--- script.js
+++ script.js
@@ -4,16 +4,16 @@ async function searchProducts() {
     resultsDiv.innerHTML = 'Идет поиск...';
 
     try {
-        const response = await fetch(`https://api.example.com/search?query=${query}`); // Замените на ваш реальный API
-        const products = await response.json();
+        const response = await axios.get(`https://skidkaonline.ru/apiv3/products/?limit=30&offset=0&shop_id=9&city_id=72&fields=id,name,name2,shops_ids,url,noted,discount_url,discount_name,date,notalladdr,image336,imagefull,brands,pricebefore,priceafter,discount_type,discount,externalurl,countPlus,countMinus,comments,desc,color,daystitle,liked,started_today,published_today&query=${query}`);
+        const products = response.data.products;
 
         resultsDiv.innerHTML = '';
         products.forEach(product => {
             const productDiv = document.createElement('div');
             productDiv.innerHTML = `
                 <h2>${product.name}</h2>
-                <p>Цена: ${product.price}</p>
-                <img src="${product.image_url}" alt="${product.name}" style="max-width: 200px;">
+                <p>Цена: ${product.priceafter} руб.</p>
+                <img src="${product.image336.src}" alt="${product.name}" style="max-width: 200px;">
             `;
             resultsDiv.appendChild(productDiv);
         });
